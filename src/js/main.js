faa7b0e278d3a0bda0b72cf602f3b02d2c59e62a
kenhua-l <l_kenhua93@hotmail.com>
2023-03-06 18:15:47 +0800

    add dengue cluster map

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
-(function() {
+$(function() {
   // everything on document ready
+  // tab functions
+  $('.tab-button').on('click', function() {
+    $(this).parent().children('.tab-button').removeClass('is-active');
+    $(this).addClass('is-active');
+    var box = $(this).data('box');
+    $('.popup-div').find('.tab-item').hide();
+    $('.popup-div').find('.tab-item[data-box=' + box + ']').show();
+  });
 
 });
